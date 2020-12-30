152c8abf425aa7597f9bd791302e62edac921755
KIM-94 <ksw2102@gmail.com>
2020-12-30 14:54:52 +0000

    connect mysql

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,46 +8,27 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/api/hello', (req, res) => {
-//     res.send({message: 'Hello Express!'});
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
 
-// });
+const connection = mysql.createConnection({
+  host: conf.host,
+  user: conf.user,
+  password: conf.password,
+  port: conf.port,
+  database: conf.database
+});
+connection.connect();
+
 
 app.get('/api/customers', (req, res) => {
-  res.send([
-    {
-      'id': 1,
-      'image': 'https://placeimg.com/64/64/1',
-      'name': '김성우1',
-      'birthday': '940401',
-      'gender': '남자',
-      'job': '대학생'
-    },
-    {
-      'id': 2,
-      'image': 'https://placeimg.com/64/64/2',
-      'name': '김성우2',
-      'birthday': '940401',
-      'gender': '남자',
-      'job': '대학생'
-    },
-    {
-      'id': 3,
-      'image': 'https://placeimg.com/64/64/3',
-      'name': '김성우3',
-      'birthday': '940401',
-      'gender': '남자',
-      'job': '대학생'
-    },
-    {
-      'id': 4,
-      'image': 'https://placeimg.com/64/64/4',
-      'name': '김성우4',
-      'birthday': '940401',
-      'gender': '남자',
-      'job': '대학생'
+  connection.query(
+    "SELECT * FROM CUSTOMER",
+    (err, rows, fields) => {
+      res.send();
     }
-  ]);
+  );
 });
 
 app.listen(port, () => console.log(`Litstening on port ${port}`)); 
